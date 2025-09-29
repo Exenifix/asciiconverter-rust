0043d75f60f21d42fc2fbb22eaa83826312251ae
Exenifix <89513380+Exenifix@users.noreply.github.com>
2025-09-29 12:05:56 +0300

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::cmp::max;
 use image::*;
 
 const CONVERSION_CHARS: [char; 7] = [' ', '.', '~', ':', '*', '#', '@'];
@@ -28,7 +27,10 @@ fn normalize_pixels(img: ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageBuffer<Luma<u8>
     })
 }
 
-fn process_image_grayscale(path: &str, resolution: f32) -> Result<String, Box<dyn std::error::Error>> {
+fn process_image_grayscale(
+    path: &str,
+    resolution: f32,
+) -> Result<String, Box<dyn std::error::Error>> {
     let img = ImageReader::open(path)?.decode()?.to_luma8();
     let (mut new_w, mut new_h) = img.dimensions();
     let ratio = 2.;
@@ -56,14 +58,20 @@ fn process_image_rgb(path: &str, resolution: f32) -> Result<String, Box<dyn std:
     new_h = (new_h as f32 * resolution / ratio) as u32;
     let resized_image = imageops::resize(&img, new_w, new_h, imageops::FilterType::Triangle);
 
-    let max_brightness = resized_image.pixels().map(|p| rgb_to_brightness(p[0], p[1], p[2])).max().unwrap_or(1) as f32;
+    let max_brightness = resized_image
+        .pixels()
+        .map(|p| rgb_to_brightness(p[0], p[1], p[2]))
+        .max()
+        .unwrap_or(1) as f32;
 
     let mut output = String::with_capacity((new_h * new_w * 20 + new_h) as usize);
     for y in 0..new_h {
         for x in 0..new_w {
             let [r, g, b] = resized_image.get_pixel(x, y).0;
             output.push_str(&rgb_to_ascii_color(r, g, b));
-            output.push(brightness_to_char((rgb_to_brightness(r, g, b) as f32 / max_brightness * 255.) as u8));
+            output.push(brightness_to_char(
+                (rgb_to_brightness(r, g, b) as f32 / max_brightness * 255.) as u8,
+            ));
         }
         output.push('\n');
     }
